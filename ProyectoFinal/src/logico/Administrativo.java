15a8203cb40ccd786d466170ddc8cb6d4932cc06
fco <fco@DESKTOP-K8IT41T>
2019-03-20 17:35:43 -0400

    Inicio Administrativo y Comercial

---
+++ ProyectoFinal/src/logico/Administrativo.java
@@ -0,0 +1,19 @@
+package logico;
+
+import java.util.Date;
+
+public class Administrativo extends Personal {
+
+	public Administrativo(String nombre, String apellido, String cedula, String telefono, String direccion, String sexo,
+			String ciudad, String telefono_fijo, String estado_social, String codigo_empleado, float sueldo_base,
+			String departamento, float sueldo, int dias_trabajados, int horas_extra, Date fecha_ingreso,
+			String num_seguro_social) {
+		super(nombre, apellido, cedula, telefono, direccion, sexo, ciudad, telefono_fijo, estado_social, codigo_empleado,
+				sueldo_base, departamento, sueldo, dias_trabajados, horas_extra, fecha_ingreso, num_seguro_social);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+
+}
--- ProyectoFinal/src/logico/Personal.java
+++ ProyectoFinal/src/logico/Personal.java
@@ -1,6 +1,6 @@
 package logico;
 
-import java.sql.Date;
+import java.util.Date;
 
 public class Personal extends Persona {
 	protected String codigo_empleado;
