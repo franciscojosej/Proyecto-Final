c9ccc0aa2aae57898fedc8eff60c111819fa1f6f
Gerardo <Gerardo@Blakki>
2019-03-20 17:29:04 -0400

    Cambio

--- ProyectoFinal/src/logico/Cliente.java
+++ ProyectoFinal/src/logico/Cliente.java
@@ -5,8 +5,17 @@ public class Cliente extends Persona {
 	private String cargo;
 	private String codigo_cliente;
 	private String codigo_tarjeta;
+	private Contrato miscontract;
 	
 	
+	public Contrato getMiscontract() {
+		return miscontract;
+	}
+
+	public void setMiscontract(Contrato miscontract) {
+		this.miscontract = miscontract;
+	}
+
 	public Cliente(String nombre, String apellido, String cedula, String telefono, String direccion, String sexo,
 			String ciudad, String telefono_fijo, String estado_social, String cargo, String codigo_cliente,
 			String codigo_tarjeta) {
