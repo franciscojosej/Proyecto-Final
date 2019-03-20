f483bea746fa4579d43ce4b126169d5c6efb0839
Gerardo <Gerardo@Blakki>
2019-03-20 17:26:48 -0400

    Contratico pa la chelcha

---
+++ ProyectoFinal/src/logico/Contrato.java
@@ -0,0 +1,55 @@
+package logico;
+
+import java.util.Date;
+
+public class Contrato {
+
+	private boolean vencida;
+	private Date fecha_de_pago;
+	private Date fecha_inicio;
+	private Date fecha_vencimiento;
+	
+	public Contrato(boolean vencida, Date fecha_de_pago, Date fecha_inicio, Date fecha_vencimiento) {
+		super();
+		this.vencida = vencida;
+		this.fecha_de_pago = fecha_de_pago;
+		this.fecha_inicio = fecha_inicio;
+		this.fecha_vencimiento = fecha_vencimiento;
+	}
+
+	public boolean isVencida() {
+		return vencida;
+	}
+
+	public void setVencida(boolean vencida) {
+		this.vencida = vencida;
+	}
+
+	public Date getFecha_de_pago() {
+		return fecha_de_pago;
+	}
+
+	public void setFecha_de_pago(Date fecha_de_pago) {
+		this.fecha_de_pago = fecha_de_pago;
+	}
+
+	public Date getFecha_inicio() {
+		return fecha_inicio;
+	}
+
+	public void setFecha_inicio(Date fecha_inicio) {
+		this.fecha_inicio = fecha_inicio;
+	}
+
+	public Date getFecha_vencimiento() {
+		return fecha_vencimiento;
+	}
+
+	public void setFecha_vencimiento(Date fecha_vencimiento) {
+		this.fecha_vencimiento = fecha_vencimiento;
+	}
+	
+	
+	
+	
+}
