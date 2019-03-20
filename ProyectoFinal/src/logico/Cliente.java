f4f1de687b2bdc2d49940754a12102a35d770325
Gerardo <Gerardo@Blakki>
2019-03-20 17:30:53 -0400

    Otro cambio

--- ProyectoFinal/src/logico/Cliente.java
+++ ProyectoFinal/src/logico/Cliente.java
@@ -1,28 +1,33 @@
 package logico;
 
+import java.util.ArrayList;
+
 public class Cliente extends Persona {
 	
 	private String cargo;
 	private String codigo_cliente;
 	private String codigo_tarjeta;
-	private Contrato miscontract;
+	private ArrayList<Contrato> miscontract;
 	
 	
-	public Contrato getMiscontract() {
-		return miscontract;
-	}
-
-	public void setMiscontract(Contrato miscontract) {
-		this.miscontract = miscontract;
-	}
+	
 
 	public Cliente(String nombre, String apellido, String cedula, String telefono, String direccion, String sexo,
 			String ciudad, String telefono_fijo, String estado_social, String cargo, String codigo_cliente,
-			String codigo_tarjeta) {
+			String codigo_tarjeta, ArrayList<Contrato> miscontract) {
 		super(nombre, apellido, cedula, telefono, direccion, sexo, ciudad, telefono_fijo, estado_social);
 		this.cargo = cargo;
 		this.codigo_cliente = codigo_cliente;
 		this.codigo_tarjeta = codigo_tarjeta;
+		this.miscontract = miscontract;
+	}
+
+	public ArrayList<Contrato> getMiscontract() {
+		return miscontract;
+	}
+
+	public void setMiscontract(ArrayList<Contrato> miscontract) {
+		this.miscontract = miscontract;
 	}
 	
 	public String getCargo() {
