bec0a4d6c5d2731635499ddbc837fca38a790d85
Gerardo <Gerardo@Blakki>
2019-03-23 11:37:18 -0400

    Clase Prueba Tricom, y cambio de codigo en Cliente

--- ProyectoFinal/src/logico/Cliente.java
+++ ProyectoFinal/src/logico/Cliente.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Cliente extends Persona {
 	
 	private String cargo;
-	private String codigo_cliente;
+	private int codigo_cliente;
 	private String codigo_tarjeta;
 	private ArrayList<Contrato> miscontract;
 	
@@ -13,7 +13,7 @@ public class Cliente extends Persona {
 	
 
 	public Cliente(String nombre, String apellido, String cedula, String telefono, String direccion, String sexo,
-			String ciudad, String telefono_fijo, String estado_social, String cargo, String codigo_cliente,
+			String ciudad, String telefono_fijo, String estado_social, String cargo, int codigo_cliente,
 			String codigo_tarjeta) {
 		super(nombre, apellido, cedula, telefono, direccion, sexo, ciudad, telefono_fijo, estado_social);
 		this.cargo = cargo;
@@ -36,10 +36,10 @@ public class Cliente extends Persona {
 	public void setCargo(String cargo) {
 		this.cargo = cargo;
 	}
-	public String getCodigo_cliente() {
+	public int getCodigo_cliente() {
 		return codigo_cliente;
 	}
-	public void setCodigo_cliente(String codigo_cliente) {
+	public void setCodigo_cliente(int codigo_cliente) {
 		this.codigo_cliente = codigo_cliente;
 	}
 	public String getCodigo_tarjeta() {
---
+++ ProyectoFinal/src/logico/Prueba_Tricom_Justo.java
@@ -0,0 +1,72 @@
+package logico;
+
+import java.util.ArrayList;
+
+
+import logico.Cliente;
+
+public class Prueba_Tricom_Justo {
+
+	private ArrayList<Cliente> miCliente;
+	private static int code;
+	
+	private Prueba_Tricom_Justo (){
+		super();
+		
+		miCliente = new ArrayList<>();
+		
+	}
+
+	public ArrayList<Cliente> getMiCliente() {
+		return miCliente;
+	}
+
+	public void setMiCliente(ArrayList<Cliente> miCliente) {
+		this.miCliente = miCliente;
+	}
+	
+	public void insertarCiente(Cliente client){
+		miCliente.add(client);
+		code++;
+		client.setCodigo_cliente(code);
+		
+	}
+	
+	public Cliente BuscarByCedula(String cedula) {
+		Cliente client=null;
+		boolean find = false;
+		int i=0;
+		while (i<miCliente.size()&&!find) {
+			if(miCliente.get(i).getCedula().equalsIgnoreCase(cedula)){
+				client = miCliente.get(i);
+				find = true;
+			}
+			i++;
+		}
+		return client;
+	}
+	
+	public void CargarDatosCliente(Cliente cli){
+		int i=0;
+		boolean found = false;
+		while (i<miCliente.size() && !found){
+			if(miCliente.get(i).getCodigo_cliente() == cli.getCodigo_cliente()){
+				miCliente.get(i).setNombre(cli.getNombre());
+				miCliente.get(i).setCedula(cli.getCedula());
+				miCliente.get(i).setTelefono(cli.getTelefono()); //miCliente.get(i).setCel(cli.getCel());
+				miCliente.get(i).setDireccion(cli.getDireccion());
+				found = true;
+			}else{
+				i++;
+			}
+		}
+	}
+	
+	public void borrarCliente(Cliente cliente){
+		miCliente.add(cliente);
+		code--;
+		cliente.setCodigo_cliente(code);
+		
+	}
+	
+}
