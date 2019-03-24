d1e88f77cb576e454219805ccfff57c1fa6b79a8
FERNANDO SANCHEZ <FERNANDO SANCHEZ@F-Sanchez-Pc>
2019-03-24 15:00:48 -0400

    vayan a ver

--- ProyectoFinal/src/logico/Contrato.java
+++ ProyectoFinal/src/logico/Contrato.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class Contrato {
 
 
-	private boolean estado;//abilidado
+	private boolean estado;//Habilidado
 	private Date fecha_de_pago;
 	private Date fecha_inicio;
 	private Date fecha_vencimiento;
