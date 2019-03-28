6e7863ac4f22270cf3adfdd95e88e568825884a0
fco <fco@DESKTOP-K8IT41T>
2019-03-28 19:03:39 -0400

    registrarEmpelado2.3

--- ProyectoFinal/src/logico/Comercial.java
+++ ProyectoFinal/src/logico/Comercial.java
@@ -6,12 +6,15 @@ public class Comercial extends Empleado {
 
 	public Comercial(String nombre, String apellido, String cedula, String telefono, String direccion, String sexo,
 			String ciudad, String telefono_fijo, String estado_social, String codigo_empleado, float sueldo_base,
-			String departamento, float sueldo, int dias_trabajados, int horas_extra, Date fecha_ingreso,
+			String departamento, float sueldo, int dias_trabajados, int horas_extra, String fecha_ingreso,
 			String num_seguro_social) {
 		super(nombre, apellido, cedula, telefono, direccion, sexo, ciudad, telefono_fijo, estado_social, codigo_empleado,
 				sueldo_base, departamento, sueldo, dias_trabajados, horas_extra, fecha_ingreso, num_seguro_social);
 		// TODO Auto-generated constructor stub
 	}
+	
+
+
 	
 	
 
